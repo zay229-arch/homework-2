0f6759b93418b5a96aa7c19e70a0535aa31060df
Corey Montella <cim310@lehigh.edu>
2023-04-06 00:12:59 +0000

    Update hashset.hpp

--- include/hashset.hpp
+++ include/hashset.hpp
@@ -40,3 +40,14 @@ class HashSet {
     void print();
 
 };
+
+/* Modify this prehash function to work with a template type T instead of a char*
+unsigned long prehash(unsigned char *str) {
+    unsigned long h = 5381;
+    int c;
+    while (c = *str++) { 
+	    h = ((h << 5) + h) + c;
+    }
+    return h;
+}*/
+
