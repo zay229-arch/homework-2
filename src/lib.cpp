02f40346d94ef7d5648a41f9e981f6f0dd8c9478
Zaya <zay229@lehigh.edu>
2026-03-25 18:17:46 -0400

    Constructor and Destructor Implementation

--- src/lib.cpp
+++ src/lib.cpp
@@ -1,9 +1,26 @@
 #include "hashset.hpp"
 
-HashSet::HashSet(size_t initial_size) {
+HashSet::HashSet(size_t initial_size)
+{
+    bucket_count = initial_size;
+    element_count = 0;
+    load_threshold = 70;
+    load_factor = 0;
+    array = new LinkedList *[bucket_count];
+    for (size_t i = 0; i < bucket_count; i++)
+    {
+        array[i] = new LinkedList();
+    }
+}
+
+HashSet::~HashSet()
+{
+    clear();
+    for (size_t i = 0; i < bucket_count; i++)
+    {
+        delete array[i];
+    }
+    delete[] array;
 
 }
 
-HashSet::~HashSet() {
-
-}
